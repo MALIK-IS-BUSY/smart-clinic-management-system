35c3e5db629b2c1077c07dbe54bfa4234212661a
Noor Fatima <noorzafar882@gmail.com>
2025-12-10 23:26:15 +0500

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace smart_clinic_management.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
