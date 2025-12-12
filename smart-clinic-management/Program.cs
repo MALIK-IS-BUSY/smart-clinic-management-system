a0569fd6ca6bd69ca0d1efe6fa4c99b78a635412
MALIK-IS-BUSY <malikabdullah0120@gmail.com>
2025-12-12 22:42:54 +0500

    bug fixes

--- smart-clinic-management/Program.cs
+++ smart-clinic-management/Program.cs
@@ -1,17 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using smart_clinic_management.Data;
-
-var builder = WebApplication.CreateBuilder(args);
-
-using Microsoft.EntityFrameworkCore;
-using smart_clinic_management.Data;
 using smart_clinic_management.Repositories;
 using smart_clinic_management.Services;
 
+var builder = WebApplication.CreateBuilder(args);
+
 // Add services to the container.
+// Register both DbContexts - ClinicDbContext for API (Entities) and ApplicationDbContext for MVC (Models)
 builder.Services.AddDbContext<ClinicDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Register repositories and services
 builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddScoped<IAppointmentService, AppointmentService>();
 builder.Services.AddScoped<IConsultationService, ConsultationService>();
@@ -20,13 +22,13 @@ builder.Services.AddScoped<IInventoryService, InventoryService>();
 builder.Services.AddScoped<IInvoiceService, InvoiceService>();
 builder.Services.AddScoped<ILowStockAlertService, LowStockAlertService>();
 
+// Add MVC services for Views
+builder.Services.AddControllersWithViews();
+
+// Add API services
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-// Add Entity Framework Core
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
@@ -50,6 +52,12 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+// Map MVC routes
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+// Map API controllers
 app.MapControllers();
 
 app.Run();
